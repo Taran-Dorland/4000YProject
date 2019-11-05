62b2b1cc93d58a27cd973f8afba843f99be8a612
Kevin Dsane-Selby <kevinlamprouch@gmail.com>
2019-11-05 18:54:36 -0500

    added login feature partially done

---
+++ app/containers/Login.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+//import logo from ""
+export class login extends React.Component {
+    constructor(props) {
+        super(props);
+
+    }
+    render() {
+        return <div className="base-container">
+<div className="header">Login</div>
+<div className="content">
+    <div className="logo">
+        <img src={login} />
+    </div>
+    <div className="form">
+    <div className="form-group">
+        <label htmlFor="username">username</label>
+        <input type="text" name="username" placeholder="username"/>
+    </div>
+    <div className="form-group">
+        <label htmlFor="password">password</label>
+        <input type="text" name="password" placeholder="password"/>
+    </div>
+</div>
+        </div>
+        <div className="form"></div>
+        <button type="button" className="btn"> </button>
+        </div>
+        
+    }
+}
+
