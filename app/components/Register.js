74840c0e71f73dbc23f28f59c654f08aa3c36e3f
Kevin <Kevinlamprouch@gmail.com>
2019-11-05 22:27:16 -0500

    Added register and style.scss started but not fully done

--- app/components/Register.js
+++ app/components/Register.js
@@ -1,21 +1,25 @@
 import React, { Component } from 'react';
 //import logo from ""
-export class login extends React.Component {
+export class Register extends React.Component {
     constructor(props) {
         super(props);
 
     }
     render() {
         return <div className="base-container">
-<div className="header">Login</div>
+<div className="header">Register</div>
 <div className="content">
     <div className="logo">
         <img src={login} />
     </div>
     <div className="form">
     <div className="form-group">
-        <label htmlFor="username">username</label>
+        <label htmlFor="username">Register</label>
         <input type="text" name="username" placeholder="username"/>
+    </div>
+    <div className="form-group">
+        <label htmlFor="email">Register</label>
+        <input type="email" name="email" placeholder="email"/>
     </div>
     <div className="form-group">
         <label htmlFor="password">password</label>
@@ -24,7 +28,7 @@ export class login extends React.Component {
 </div>
         </div>
         <div className="form"></div>
-        <button type="button" className="btn"> </button>
+        <button type="button" className="btn">Login </button>
         </div>
         
     }
