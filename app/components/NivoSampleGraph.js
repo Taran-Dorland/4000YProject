fe13d5842b1abd584e24d27cf94aa11e6f67577d
Taran <tarandorland@protonmail.com>
2020-01-14 17:52:05 -0500

    Added graph filters and setup for dynamic graph

--- app/components/NivoSampleGraph.js
+++ app/components/NivoSampleGraph.js
@@ -1,7 +1,7 @@
 // @flow
 import React, { Component } from 'react';
 import { ResponsiveBar } from '@nivo/bar'
-import { Checkbox, Select } from 'antd';
+import { Checkbox, Select, Tabs } from 'antd';
 
 type Props = {};
 
@@ -21,32 +21,41 @@ export default class NivoSampleGraph extends Component<Props> {
 
     var thisData = this.props.csvData;
 
-    function onChange(checkedValues) {
-      console.log('checked = ', checkedValues);
-    }
-
-    function handleChange(value) {
-      console.log(`selected ${value}`);
-    }
-    
-    const plainOptions = ['Apple', 'Pear', 'Orange'];
-    const options = [
-      { label: 'Apple', value: 'Apple' },
-      { label: 'Pear', value: 'Pear' },
-      { label: 'Orange', value: 'Orange' },
-    ];
-    const optionsWithDisabled = [
-      { label: 'Apple', value: 'Apple' },
-      { label: 'Pear', value: 'Pear' },
-      { label: 'Orange', value: 'Orange', disabled: false },
-    ];
-
     const { Option } = Select;
 
+    //Options for the checkbox group
+    const angerOptions = [];
+    const cogOptions = [];
+    const eduOptions = [];
+    const employmentOptions = [];
+    const substanceOptions = [];
+    const otherOptions = [];
+    for (let i = 5; i < thisData[0].length; i++) {
+
+      if (thisData[0][i].toUpperCase().substring(0, 5) === "ANGER") {
+        angerOptions.push(<Option key={thisData[0][i].toString(36)}>{thisData[0][i].toString(36)}</Option>);
+      } else if (thisData[0][i].toUpperCase().substring(0, 3) === "COG") {
+        cogOptions.push(<Option key={thisData[0][i].toString(36)}>{thisData[0][i].toString(36)}</Option>);
+      } else if (thisData[0][i].toUpperCase().substring(0, 9) === "EDUCATION") {
+        eduOptions.push(<Option key={thisData[0][i].toString(36)}>{thisData[0][i].toString(36)}</Option>);
+      } else if (thisData[0][i].toUpperCase().substring(0, 10) === "EMPLOYMENT") {
+        employmentOptions.push(<Option key={thisData[0][i].toString(36)}>{thisData[0][i].toString(36)}</Option>);
+      } else if (thisData[0][i].toUpperCase().substring(0, 9) === "SUBSTANCE") {
+        substanceOptions.push(<Option key={thisData[0][i].toString(36)}>{thisData[0][i].toString(36)}</Option>);
+      } else {
+        otherOptions.push(<Option key={thisData[0][i].toString(36)}>{thisData[0][i].toString(36)}</Option>);
+      }
+    }
+
+    //Options for the select box
     const children = [];
     for (let i = 1; i < thisData.length; i++) {
 
-      children.push(<Option key={thisData[i][0]}></Option>);
+      if (!thisData[i][0]) {
+        thisData[i][0] = "no name specified" + i.toString();
+      }
+
+      children.push(<Option key={thisData[i][0].toString(36)}>{thisData[i][0].toString(36)}</Option>);
       console.log("Pushed: " + thisData[i][0]);
     }
 
@@ -90,33 +99,28 @@ export default class NivoSampleGraph extends Component<Props> {
 
     var dataKeys = [ thisData[0][5], thisData[0][6], thisData[0][65], thisData[0][66] ];
 
-    return (
+    function handleChange(value) {
 
-        <div style={{ height: 500, width: 1000 }}>
-        <Checkbox.Group options={plainOptions} defaultValue={['Apple']} onChange={onChange} />
-        <br />
-        <br />
-        <Checkbox.Group options={options} defaultValue={['Pear']} onChange={onChange} />
-        <br />
-        <br />
-        <Checkbox.Group
-          options={optionsWithDisabled}
-          disabled
-          defaultValue={['Apple']}
-          onChange={onChange}
-        />
-        
-        <Select
-          mode="multiple"
-          style={{ width: '75%' }}
-          placeholder="Please select"
-          defaultValue={[]}
-          onChange={handleChange}
-        >
-        {children}
-        </Select>
 
-        <ResponsiveBar
+
+      console.log(`selected ${value}`);
+    }
+
+    function handleNameChange(value) {
+
+      for (let i = 0; i < value.length; i++) {
+
+        var dataToAdd = {
+          "Client Name": value[i],
+        }
+
+        data.push(dataToAdd);
+      }
+      console.log(`selected ${value}`);
+    }
+
+    const MyResponsiveBar = ({ data }) => (
+      <ResponsiveBar
         data={data}
         keys={dataKeys}
         indexBy="Client Name"
@@ -193,7 +197,65 @@ export default class NivoSampleGraph extends Component<Props> {
         animate={true}
         motionStiffness={90}
         motionDamping={15}
-    />
+        />
+    )
+
+    return (
+
+        <div style={{ height: 500, width: 1000 }}>
+
+        <Select
+          mode="multiple"
+          style={{ width: '50%' }}
+          placeholder="Please select"
+          onChange={handleChange}
+        >
+        {angerOptions}
+        </Select>
+        <Select
+          mode="multiple"
+          style={{ width: '50%' }}
+          placeholder="Please select"
+          onChange={handleChange}
+        >
+        {cogOptions}
+        </Select>
+        <Select
+          mode="multiple"
+          style={{ width: '50%' }}
+          placeholder="Please select"
+          onChange={handleChange}
+        >
+        {eduOptions}
+        </Select>
+        <Select
+          mode="multiple"
+          style={{ width: '50%' }}
+          placeholder="Please select"
+          onChange={handleChange}
+        >
+        {employmentOptions}
+        </Select>
+        <Select
+          mode="multiple"
+          style={{ width: '50%' }}
+          placeholder="Please select"
+          onChange={handleChange}
+        >
+        {otherOptions}
+        </Select>
+
+        
+        <Select
+          mode="multiple"
+          style={{ width: '50%' }}
+          placeholder="Please select"
+          onChange={handleNameChange}
+        >
+        {children}
+        </Select>
+
+        <MyResponsiveBar data={data} />
         </div>
     );
   }
